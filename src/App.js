016aff495d39f653d68404fb5cb85ddf636931a4
Evan Anderson <evndanderson@gmail.com>
2022-12-27 00:07:17 -0600

    Initial commit Added wav files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
+import * as wavs from './wavs.js';
 import './App.css';
 
 function App() {
+  const n = 20;
+  const rows = [];
+
+  for (let i = 0; i < n; i++) {
+    // Offset is the width of the box for each subsequent row
+    const duration = (n / (n - i)) * 1;
+
+    rows.push(
+      <div key={i} className='row' style={{'width': `${n * 50}px`}}>
+        <div id={`box_${i}`} className='box' style={{
+            "animationDuration": `${duration}s`,
+            "backgroundColor": `rgb(${i / n * 255}, ${255 - (i / n * 255)}, 255)`
+          }}>
+        </div>
+      </div>
+    );
+
+    // Need to wait for App.js return
+    window.addEventListener('load', () => {
+      // Add event listener for each collision
+      document.getElementById(`box_${i}`).addEventListener('animationiteration', (e) => {
+        const audio = new Audio(wavs[`pl_${n - i}`]);
+        audio.volume = 0.25;
+        audio.play();
+      });
+    });
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='row-container' style={{'height': `${n * 50}px`}}>
+        {rows}
+      </div>
     </div>
   );
 }
---
+++ src/wavs.js
@@ -0,0 +1,45 @@
+// There has got to be a better way to do this
+import pl_0 from './wav/pl_0.wav';
+import pl_1 from './wav/pl_1.wav';
+import pl_2 from './wav/pl_2.wav';
+import pl_3 from './wav/pl_3.wav';
+import pl_4 from './wav/pl_4.wav';
+import pl_5 from './wav/pl_5.wav';
+import pl_6 from './wav/pl_6.wav';
+import pl_7 from './wav/pl_7.wav';
+import pl_8 from './wav/pl_8.wav';
+import pl_9 from './wav/pl_9.wav';
+import pl_10 from './wav/pl_10.wav';
+import pl_11 from './wav/pl_11.wav';
+import pl_12 from './wav/pl_12.wav';
+import pl_13 from './wav/pl_13.wav';
+import pl_14 from './wav/pl_14.wav';
+import pl_15 from './wav/pl_15.wav';
+import pl_16 from './wav/pl_16.wav';
+import pl_17 from './wav/pl_17.wav';
+import pl_18 from './wav/pl_18.wav';
+import pl_19 from './wav/pl_19.wav';
+
+export { 
+    pl_0,
+    pl_1,
+    pl_2,
+    pl_3,
+    pl_4,
+    pl_5,
+    pl_6,
+    pl_7,
+    pl_8,
+    pl_9,
+    pl_10,
+    pl_11,
+    pl_12,
+    pl_13,
+    pl_14,
+    pl_15,
+    pl_16,
+    pl_17,
+    pl_18,
+    pl_19,
+ }
+ 
